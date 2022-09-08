e1010981731187201eada35a8b108867ba07b86a
taskswithcode <farminaround@gmail.com>
2022-09-08 17:51:00 +0000

    Added

--- twc_embeddings.py
+++ twc_embeddings.py
@@ -1,69 +1,95 @@
 import torch
 from transformers import AutoModel, AutoTokenizer
 from scipy.spatial.distance import cosine
+import argparse
+import json
 
-# Get our models - The package will take care of downloading the models automatically
-# For best performance: Muennighoff/SGPT-5.8B-weightedmean-nli-bitfit
-tokenizer = AutoTokenizer.from_pretrained("Muennighoff/SGPT-125M-weightedmean-nli-bitfit")
-model = AutoModel.from_pretrained("Muennighoff/SGPT-125M-weightedmean-nli-bitfit")
-# Deactivate Dropout (There is no dropout in the above models so it makes no difference here but other SGPT models may have dropout)
-model.eval()
+def read_text(input_file):
+    arr = open(input_file).read().split("\n")
+    return arr[:-1]
 
-# Tokenize input texts
-texts = [
-    "deep learning",
-    "Deep Learning",
-    "natural language processing",
-    "NLP",
-    "ATM",
-    "deep fakes",
-    "AGI",
-    "AI",
-    "deep trouble",
-    "artificial intelligence",
-    "deep diving",
-    "artificial snow",
-    "Transfomers have become the staple architecture for deep learning models",
-    "shallow waters",
-    "deep end"
-]
-batch_tokens = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
 
-# Get the embeddings
-with torch.no_grad():
-    # Get hidden state of shape [bs, seq_len, hid_dim]
-    last_hidden_state = model(**batch_tokens, output_hidden_states=True, return_dict=True).last_hidden_state
+class SGPTModel:
+    def __init__(self):
+        self.model = None
+        self.tokenizer = None
 
-# Get weights of shape [bs, seq_len, hid_dim]
-weights = (
-    torch.arange(start=1, end=last_hidden_state.shape[1] + 1)
-    .unsqueeze(0)
-    .unsqueeze(-1)
-    .expand(last_hidden_state.size())
-    .float().to(last_hidden_state.device)
-)
 
-# Get attn mask of shape [bs, seq_len, hid_dim]
-input_mask_expanded = (
-    batch_tokens["attention_mask"]
-    .unsqueeze(-1)
-    .expand(last_hidden_state.size())
-    .float()
-)
+    def init_model(self):
+        # Get our models - The package will take care of downloading the models automatically
+        # For best performance: Muennighoff/SGPT-5.8B-weightedmean-nli-bitfit
+        #self.tokenizer = AutoTokenizer.from_pretrained("Muennighoff/SGPT-125M-weightedmean-nli-bitfit")
+        #self.model = AutoModel.from_pretrained("Muennighoff/SGPT-125M-weightedmean-nli-bitfit")
+        self.tokenizer = AutoTokenizer.from_pretrained("Muennighoff/SGPT-1.3B-weightedmean-msmarco-specb-bitfit")
+        self.model = AutoModel.from_pretrained("Muennighoff/SGPT-1.3B-weightedmean-msmarco-specb-bitfit")
+        #self.tokenizer = AutoTokenizer.from_pretrained("Muennighoff/SGPT-5.8B-weightedmean-msmarco-specb-bitfit")
+        #self.model = AutoModel.from_pretrained("Muennighoff/SGPT-5.8B-weightedmean-msmarco-specb-bitfit")
+        # Deactivate Dropout (There is no dropout in the above models so it makes no difference here but other SGPT models may have dropout)
+        self.model.eval()
 
-# Perform weighted mean pooling across seq_len: bs, seq_len, hidden_dim -> bs, hidden_dim
-sum_embeddings = torch.sum(last_hidden_state * input_mask_expanded * weights, dim=1)
-sum_mask = torch.sum(input_mask_expanded * weights, dim=1)
+    def compute_embeddings(self,input_data,is_file):
+        model = self.model
+        tokenizer = self.tokenizer
 
-embeddings = sum_embeddings / sum_mask
+        texts = read_text(input_data) if is_file == True else input_data
 
-# Calculate cosine similarities
-# Cosine similarities are in [-1, 1]. Higher means more similar
-cosine_dict = {}
-for i in range(len(texts)):
-        cosine_dict[texts[i]] = 1 - cosine(embeddings[0], embeddings[i])
+        # Tokenize input texts
+        batch_tokens = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
 
-print("Input sentence:",texts[0])
-sorted_dict = dict(sorted(cosine_dict.items(), key=lambda item: item[1],reverse = True))
-for key in sorted_dict:
-    print("Cosine similarity with  \"%s\" is: %.3f" % (key, sorted_dict[key]))
+    # Get the embeddings
+        with torch.no_grad():
+            # Get hidden state of shape [bs, seq_len, hid_dim]
+            last_hidden_state = model(**batch_tokens, output_hidden_states=True, return_dict=True).last_hidden_state
+
+        # Get weights of shape [bs, seq_len, hid_dim]
+        weights = (
+            torch.arange(start=1, end=last_hidden_state.shape[1] + 1)
+            .unsqueeze(0)
+            .unsqueeze(-1)
+            .expand(last_hidden_state.size())
+            .float().to(last_hidden_state.device)
+        )
+
+        # Get attn mask of shape [bs, seq_len, hid_dim]
+        input_mask_expanded = (
+            batch_tokens["attention_mask"]
+            .unsqueeze(-1)
+            .expand(last_hidden_state.size())
+            .float()
+        )
+
+        # Perform weighted mean pooling across seq_len: bs, seq_len, hidden_dim -> bs, hidden_dim
+        sum_embeddings = torch.sum(last_hidden_state * input_mask_expanded * weights, dim=1)
+        sum_mask = torch.sum(input_mask_expanded * weights, dim=1)
+
+        embeddings = sum_embeddings / sum_mask
+        return texts,embeddings
+
+    def output_results(self,output_file,texts,embeddings):
+        # Calculate cosine similarities
+        # Cosine similarities are in [-1, 1]. Higher means more similar
+        cosine_dict = {}
+        print("Total sentences",len(texts))
+        for i in range(len(texts)):
+                cosine_dict[texts[i]] = 1 - cosine(embeddings[0], embeddings[i])
+
+        print("Input sentence:",texts[0])
+        sorted_dict = dict(sorted(cosine_dict.items(), key=lambda item: item[1],reverse = True))
+        for key in sorted_dict:
+            print("Cosine similarity with  \"%s\" is: %.3f" % (key, sorted_dict[key]))
+        if (output_file is not None):
+            with open(output_file,"w") as fp:
+                fp.write(json.dumps(sorted_dict))
+        return sorted_dict
+
+
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='SGPT model for sentence embeddings ',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument('-input', action="store", dest="input",required=True,help="Input file with sentences")
+        parser.add_argument('-output', action="store", dest="output",default="output.json",help="Output file with results")
+        results = parser.parse_args()
+        obj = SGPTModel()
+        obj.init_model()
+        texts, embeddings = obj.compute_embeddings(results.input,is_file = True)
+        results = obj.output_results(results.output,texts,embeddings)
